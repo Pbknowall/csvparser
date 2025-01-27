2b9677842f5abea54bc02c3a9dd0e267f317ca45
Pbknowall <pbknowall2@gmail.com>
2025-01-27 23:26:46 +0000

    Now works, just need to add path, file format, and file name format customisation

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import csv
 
 #file = input('Enter the name of the playlist file:')
-file = 'C:/Users/Pbknowall/Desktop/Liked Songs - Skiley Export.csv'
+file = 'D:/Music/iTunes/Playlists/house.csv'
+fileFormat = 'mp3'
 if file.endswith('.csv'):
     with open(file, 'r', encoding="utf8") as f:
         reader = csv.DictReader(f)
@@ -11,33 +12,44 @@ else: jsonData = open(file()).read()
 
 def proofCheckTrackInfo(data):
     keyVariations = {
-            "trackName": ["trackname", "track name", "name"],
-            "artistName": ["artistname", "artist name", "artist"],
-            "duration": ["duration", "length", "time"]
+            "trackName": ["trackname", "name"],
+            "artistName": ["artistname", "artist", "artistname(s)"],
+            "duration": ["duration", "length", "time", "duration(ms)"]
         }
-    normalisedKeys = {key.lower().replace(" ", ""): value for key, value in data.items()}
+    normalisedData = []
+    for element in data:
+        normalisedData.append({key.lower().replace(" ", ""): value for key, value in element.items()})
     missingKeys = []
+    for canKey, variations in keyVariations.items():
+        if not any(var in normalisedData[0] for var in variations):
+            missingKeys.append(canKey)
 
     for canKey, variations in keyVariations.items():
-        if not any(var in normalisedKeys for var in variations):
-            missingKeys.append(canKey)
+        for variation in variations:
+            for element in normalisedData:
+                if variation in element:
+                    element[canKey] = element.pop(variation)
     
     if missingKeys:
-        return False, missingKeys
-    return True
+        return False, normalisedData, missingKeys
+    return (True, normalisedData, [])
 
-res = proofCheckTrackInfo(jsonData[0])
-if len(res[1]) > 0 and 'duration' not in res[1]:
-    print('The following keys are missing from the file provided:', ', '.join(res[1]))
+
+
+res = proofCheckTrackInfo(jsonData)
+print(res)
+
+if len(res[2]) > 1 or (len(res[2]) > 0 and 'duration' not in res[2]):
+    print('The following keys are missing from the file provided:', ', '.join(res[2]))
 else:
-    if len(res[1]) > 0:
+    if 'duration' in res[2]:
         print('***Duration is not a key in the file provided, meaning some media players may not support the m3u8 to be created***\n***Proper synchronisation and seemless playback may not be possible and the playlist\'s total duration may not be displayed***')
-
     #name = file.split('/')[-1].split('.')[0] if file.count('/')>file.count('\\') else file.split('\\')[-1].split('.')[0]
     with open(file.replace('.csv', '.m3u8').replace('.json', '.m3u8'), 'w') as f:
         f.write('#EXTM3U\n')
-        for track in jsonData:
+        for track in res[1]:
+            #print(track)
             duration = track.get("duration", -1)
-            print(duration)
-            f.write(f'#EXTINF:{duration},{track["artistName"]} - {track["trackName"]}\n') # Implement different formats
-            #f.write(f'{track["trackUrl"]}\n')
+            #print(duration)
+            f.write(f'#EXTINF:{duration if int(duration)<1000 else int(int(duration)/1000)},{track["artistName"]} - {track["trackName"]}\n') # Implement different formats
+            f.write(f'{track["trackName"]}.{fileFormat}\n')
